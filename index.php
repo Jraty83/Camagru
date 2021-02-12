0b361ae61f3e8b6f240d7c74a44562562350495c
jraty <jraty@student.hive.fi>
2021-02-12 17:53:59 +0200

    require once added/tested

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <title>Index</title>
 <?php
-include 'config/setup.php';
+require_once './config/setup.php';
+//include 'config/setup.php';
 //include 'login.php';
 
 echo "! ! ! LOGGED IN USERS ONLY ! ! !";

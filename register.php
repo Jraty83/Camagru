bae5e83a8f479971ab9f893240c7c81a0e82b16c
jraty <jraty@student.hive.fi>
2021-02-15 10:14:44 +0200

    errors array draft added

--- register.php
+++ register.php
@@ -1,5 +1,9 @@
 <?php
+include './temp/insert_into.php';
 
+$errors = [];
+// array_push($errors,"keke<br>");
+// array_push($errors,"orava");
 ?>
 
 <head>
@@ -37,6 +41,8 @@
 	<div>
 		<input type="submit" name="submit" value="Register">
 	</div>
+	<p class="err"><?php foreach ($errors as $err_msg)
+	echo $err_msg; ?></p>
 	<div>
 		<p>Already have an account? Back to login<br>
 		<a href="login.php">Login</a></p>

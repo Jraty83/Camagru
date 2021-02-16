1a66125783c339ee3de5f421cfc320fd383cc950
jraty <jraty@student.hive.fi>
2021-02-16 14:33:10 +0200

    register validation polished

--- register.php
+++ register.php
@@ -1,50 +1,60 @@
 <?php
-include './temp/insert_into.php';
+require_once 'config/setup.php';
+include 'temp/validate_reg.php';
 
-$errors = [];
-// array_push($errors,"keke<br>");
-// array_push($errors,"orava");
+if ($valid_input == 4 && !$duplicate) {
+	//TODO - NOW INSERT INTO DATABE and goto LOGIN.PHP
+	echo "JEEEEEE";
+
+}
+
 ?>
 
-<head>
-  <title>Register new user</title>
-</head>
-<style>
-	<?php include 'main.css'; ?>
-</style>
+<title>Register new user</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="style/cam.png?">
+<link rel="stylesheet" href="style/main.css">
 
-<form name='registration' action='./temp/insert_into.php' method='POST'>
+<form name="registration" action="" method="post">
 	<!-- <div> -->
 		<label>Username:</label>
 		<div>
-			<input type="text" name="username" placeholder="enter username" value="" />
+			<input type="text" name="username" placeholder="enter username" value="<?php echo $_POST['username']?>" />
 		</div>
 	<!-- </div> -->
 	<!-- <div> -->
 		<label>Email:</label>
 		<div>
-			<input type="email" name="email" placeholder="enter email" value="" />
+			<input type="email" name="email" placeholder="enter email" value="<?php echo $_POST['email']?>" />
 		</div>
 	<!-- </div> -->
 	<!-- <div> -->
 		<label>Password:</label>
 		<div>
-			<input type="password" name="password" placeholder="enter password" value="" />
+			<input type="password" name="password" placeholder="enter password" />
+			<text class="info">*min. 8 characters. at least one uppercase, lowercase & digit or special character</text>
 		</div>
 	<!-- </div> -->
 	<!-- <div> -->
 		<label>Confirm password:</label>
 		<div>
-			<input type="password" name="password2" placeholder="enter password" value="" />
+			<input type="password" name="password2" placeholder="re-enter password" />
 		</div>
 	<!-- </div> -->
 	<div>
 		<input type="submit" name="submit" value="Register">
 	</div>
-	<p class="err"><?php foreach ($errors as $err_msg)
-	echo $err_msg; ?></p>
-	<div>
-		<p>Already have an account? Back to login<br>
-		<a href="login.php">Login</a></p>
-	</div>
 </form>
+<div>
+	<ul>
+		<?php if (count($errors) > 0)
+			foreach ($errors as $err_msg)
+				echo '<li class="err">' . $err_msg . "</li>"?>
+	</ul>
+</div>
+<div>
+	<p>Already have an account? Back to login<br>
+	<a href="login.php">Login</a></p>
+</div>
+<?php require_once 'footer.php';?>
--- temp/validate_reg.php
+++ temp/validate_reg.php
@@ -1,31 +1,58 @@
 <?php
 
-function validate_reg($user,$email,$password) {
-	$password2 = $_POST['password2'];
-	//REGEX FOR PASSWORD
-	$upper = preg_match('@[A-Z]@', $password);
-	$lower = preg_match('@[a-z]@', $password);
-	$digit = preg_match('@[0-9]@', $password);
-	$special = preg_match('@\W@', $password);
-	$minlen = strlen($password);
+$errors = [];
+$valid_input = 0;
+$duplicate = false;
 
-	if ($_POST['submit'] == "Register") {
-		if ($user && (trim($user) != "")) {
-			if ($email) {
-				if ($password === $password2) {
-					if($upper && $lower && $digit && $special && ($minlen >= 8))
-						return true;
-					else
-						echo "your password must contain at least:<br><li>8 characters<br><li>one Uppercase<br><li>one Lowercase<br><li>one digit &<br><li>one special character<br>";
-				}
-				else
-					echo "passwords don't match";
-			}
-			else
-				echo "enter an email address";
-		}
-		else
-			echo "enter a username";
+$user = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password2 = $_POST['password2'];
+
+// QUERY FOR CHECKING EXISTING USERS / EMAILS
+$stmt = $conn->prepare("SELECT * FROM users");
+$stmt->execute();
+$data = $stmt->fetchAll();
+
+foreach ($data as $row) {
+	if ($row['username'] === $user) {
+		$duplicate = true;
+		array_push($errors,"username already used, choose another one");
+	}
+	if ($row['email'] === $email) {
+		$duplicate = true;
+		array_push($errors,"email already used, choose another one");
 	}
 }
+
+//REGEX FOR PASSWORD
+$upper = preg_match('@[A-Z]@', $password);
+$lower = preg_match('@[a-z]@', $password);
+$digit = preg_match('@[0-9]@', $password);
+$special = preg_match('@\W@', $password);
+$minlen = strlen($password);
+
+if ($_POST['submit'] === "Register") {
+	if ($user && (trim($user) != ""))
+		$valid_input++;
+	else
+		array_push($errors,"enter a username");
+	if ($email)
+		$valid_input++;
+	else
+		array_push($errors,"enter an email address");
+	if ($upper && $lower && ($digit||$special) && ($minlen >= 8))
+		$valid_input++;
+	else
+		array_push($errors,"enter a valid password");
+	if (!$password2)
+		array_push($errors,"re-enter password");
+	if ($password && $password2) {
+		if ($password === $password2)
+			$valid_input++;
+		else
+			array_push($errors,"passwords don't match");
+	}
+}
+
 ?>

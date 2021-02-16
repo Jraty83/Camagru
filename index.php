9607f2a801894971f002940a2a98f857e3c509b2
jraty <jraty@student.hive.fi>
2021-02-16 14:32:19 +0200

    some styling added

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
-<title>Index</title>
 <?php
-require_once './config/setup.php';
-//include 'config/setup.php';
-//include 'login.php';
+//require_once './config/setup.php';
+
+?>
+
+<title>Index</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="style/cam.png?">
+<link rel="stylesheet" href="style/main.css">
+
+<?php
 
 echo "! ! ! LOGGED IN USERS ONLY ! ! !";
 
 ?>
+<?php require_once 'footer.php';?>

0cbfa6fa2d45de40a169cd3fc06f1402f947916d
jraty <jraty@student.hive.fi>
2021-03-05 12:09:36 +0200

    webcam setup done, some minor tuning needed

--- index.php
+++ index.php
@@ -14,23 +14,33 @@ $user = $_SESSION['user'];
 		<link rel="icon" href="includes/cam.png?">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 		<link rel="stylesheet" href="includes/main.css">
+		<link rel="stylesheet" href="includes/webcam.css" type="text/css" media="all">
+		<script src="includes/takepic.js">
+		</script>
 	</head>
 	<body>
-		<?php require_once 'includes/navbar.php';?>
-		
-		<?php
+		<?php require_once 'includes/navbar.php';
 
+		// FOR LOGGED IN USER'S ONLY
 		if ($user) {
-			echo "Logged in as: " . $user;
-			echo '<h1 class="access">! ! ! WELCOME TO CAMAGRU ! ! !</h1>';
-			echo '<form action="">
-			<input type="submit" id="logout" name="logout" value="Test Button" >
-			</form>';
-		}
-		else
-			echo '<h1 class="denied">! ! ! VERIFIED ACCOUNTS ACCESS ONLY ! ! !</h1>';
-		?>
-		<?php require_once 'includes/footer.php';?>
+			echo "<p class='right-align'>Logged in as: ".$user."</p>"?>
+			<div class="camera">
+				<video id="video">Video stream not available.</video>
+				<button id="startbutton">Take photo</button>
+			</div>
+			<canvas id="canvas">
+			</canvas>
+			<div class="output">
+				<img id="photo" alt="The screen capture will appear in this box.">
+			</div>
+		<?php }
+
+		// FOR EVERYBODY
+		else { ?>
+			<h1 class="denied">! ! ! GALLERY ONLY ! ! !</h1>
+		<?php }
+
+		require_once 'includes/footer.php';?>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 	</body>
 </html>

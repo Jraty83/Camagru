2b7107e6795a0ed9ed2036edade223e6e90a8fb3
jraty <jraty@student.hive.fi>
2021-03-05 12:37:50 +0200

    console error fixed by moving script head->body

--- index.php
+++ index.php
@@ -15,8 +15,6 @@ $user = $_SESSION['user'];
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 		<link rel="stylesheet" href="includes/main.css">
 		<link rel="stylesheet" href="includes/webcam.css" type="text/css" media="all">
-		<script src="includes/takepic.js">
-		</script>
 	</head>
 	<body>
 		<?php require_once 'includes/navbar.php';
@@ -24,6 +22,7 @@ $user = $_SESSION['user'];
 		// FOR LOGGED IN USER'S ONLY
 		if ($user) {
 			echo "<p class='right-align'>Logged in as: ".$user."</p>"?>
+			<script src="includes/takepic.js"></script>
 			<div class="camera">
 				<video id="video">Video stream not available.</video>
 				<button id="startbutton">Take photo</button>

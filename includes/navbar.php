2025a951134f9992303e55eed762b9d7b91223c0
jraty <jraty@student.hive.fi>
2021-03-08 11:33:03 +0200

    improved navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,28 +7,23 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <?php if (!$_SESSION['user']) {
-        echo
-        '<li class="nav-item">
-        <!-- <a class="nav-link" href="user/login.php">Login</a> -->
-          <a class="nav-link" href="http://localhost:8080/camagru/user/login.php">Login</a>
-        </li>';
-        echo
-        '<li class="nav-item">
-          <!-- <a class="nav-link active" aria-current="page" href="user/register.php">Sign Up</a> -->
-          <a class="nav-link active" aria-current="page" href="http://localhost:8080/camagru/user/register.php">Sign Up</a>
-        </li>'; }
-        else {
-        echo
-        '<li class="nav-item">
-          <!-- <a class="nav-link" href="user/logout.php">Account Settings</a> -->
-          <a class="nav-link" href="http://localhost:8080/camagru/user/settings.php">Account Settings</a>
-        </li>';
-        echo
-        '<li class="nav-item">
-        <!-- <a class="nav-link" href="user/logout.php">Logout</a> -->
-        <a class="nav-link" href="http://localhost:8080/camagru/user/logout.php">Logout</a>
-        </li>'; } ?>
+        <?php if ($_SESSION['user']) { ?>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="user/logout.php">Account Settings</a> -->
+            <a class="nav-link" href="http://localhost:8080/camagru/user/settings.php">Account Settings</a>
+          </li>
+          <li class="nav-item">
+          <!-- <a class="nav-link" href="user/logout.php">Logout</a> -->
+          <a class="nav-link" href="http://localhost:8080/camagru/user/logout.php">Logout</a>
+          <?php } else { ?>
+            <li class="nav-item">
+            <!-- <a class="nav-link" href="user/login.php">Login</a> -->
+            <a class="nav-link" href="http://localhost:8080/camagru/user/login.php">Login</a>
+            </li>
+            <li class="nav-item">
+            <!-- <a class="nav-link active" aria-current="page" href="user/register.php">Sign Up</a> -->
+            <a class="nav-link active" aria-current="page" href="http://localhost:8080/camagru/user/register.php">Sign Up</a>
+            </li> <?php } ?>
         <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown

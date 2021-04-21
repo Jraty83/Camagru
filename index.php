9f81a38036e95c6d2cd01747fa4adfe88ca5cd8d
jraty <jraty@student.hive.fi>
2021-04-21 15:46:12 +0300

    pagination now works, but needs a serious cleanup

--- index.php
+++ index.php
@@ -7,6 +7,39 @@ require_once 'admin/mail.php';
 
 $user = $_SESSION['user'];
 
+//todo PAGINATION CALCULATIONS START HERE (extract to another file, maybe use with webcam also?)
+$per_page = 6;
+
+// Get the page and offset value:
+if (isset($_GET['page'])) {
+	$page = $_GET['page'] - 1;
+	$offset = $page * $per_page;
+}
+// Otherwise we render the page and offset as non-existent:
+else {
+	$page = 0;
+	$offset = 0;
+}
+
+// Count the total number of pictures:
+$stmt = $conn->prepare("SELECT * FROM pictures ORDER BY img_id DESC");
+$stmt->execute();
+$total_pictures = $stmt->rowCount();
+
+// Calculate the number of pages:
+if ($total_pictures > $per_page) { // If there is more than one page
+	$pages_total = ceil($total_pictures / $per_page);
+	$page_up = $page + 2;
+	$page_down = $page;
+	$display = ''; // leave the display variable empty so it doesn't hide anything
+}
+else { // Else if there is only one page
+	$pages = 1;
+	$pages_total = 1;
+	$display = ' class="display-none"'; // class to hide page count and buttons if only one page
+}
+//todo PAGINATION CALCULATIONS END HERE
+
 if(isset($_POST['like'])) {
 	$img_id = $_POST['like'];
 	$stmt = $conn->prepare("UPDATE pictures SET likes = likes + 1 WHERE $img_id = img_id");
@@ -96,19 +129,51 @@ if ($_POST['msg_submit'] === "Post" && $_POST['comment']) {
 			<p class="logged">Logged in as: <?php echo $user?></p>
 		<?php }
 
-		$stmt = $conn->prepare("SELECT * FROM pictures ORDER BY img_id DESC");
+		// $stmt = $conn->prepare("SELECT * FROM pictures ORDER BY img_id DESC");
+		// $stmt->execute();
+		// $pics = $stmt->fetchAll();
+		// $count = $stmt->rowCount();
+		// if ($count > 0) {
+			echo '<label style="margin-left: 10px;font-weight:bold">Total of '.$total_pictures.' images.</label><br>';
+			echo '<h3 style="margin-left: 10px;font-weight:bold"'.$display.'>Page '; echo $page + 1 .' of '.$pages_total.'</h3>';// Page out of total pages
+			// echo "<br><br>";
+		// }
+		$i = 1; // Set the $i counting variable to 1
+		echo '<div id="pageNav"'.$display.'>'; // our $display variable will do nothing if more than one page
+
+			// Show the page buttons:
+			if ($page) {
+				echo '<a href="index.php"><button><<</button></a>'; // Button for first page [<<]
+				echo '<a href="index.php?page='.$page_down.'"><button><</button></a>'; // Button for previous page [<]
+			}
+
+			for ($i = 1; $i <= $pages_total; $i++) {
+				if (($i == $page + 1)) {
+					echo '<a href="index.php?page='.$i.'"><button class="active">'.$i.'</button></a>'; // Button for active page, underlined using 'active' class
+				}
+
+				// In this next if statement, calculate how many buttons you'd like to show. You can remove to show only the active button and first, prev, next and last buttons:
+				if (($i != $page + 1) && ($i <= $page + 3) && ($i >= $page - 1)) { // This is set for two below and two above the current page
+					echo '<a href="index.php?page='.$i.'"><button>'.$i.'</button></a>';
+				}
+			}
+
+		
+			if (($page + 1) != $pages_total) {
+				echo '<a href="index.php?page='.$page_up.'"><button>></button></a>'; // Button for next page [>]
+				echo '<a href="index.php?page='.$pages_total.'"><button>>></button></a>'; // Button for last page [>>]
+			}
+
+		echo '</div>'; // #pageNav end
+
+		echo '<div id="gallery">';// #gallery div to contain the gallery
+
+		// SHOW ALL PICTURES
+		$stmt = $conn->prepare("SELECT * FROM pictures ORDER BY img_id DESC LIMIT $offset, $per_page");
 		$stmt->execute();
 		$pics = $stmt->fetchAll();
-		$count = $stmt->rowCount();
-		if ($count > 0) { ?>
-			<label style="margin-left: 10px;font-weight:bold">Total of <?php echo $count?> images.</label>
-			<br>
-			<br>
-		<?php }
-
-		// SHOW ALL PICTURES ?>
-		<div class="row">
-			<?php
+		
+		echo '<div class="row">';
 			foreach ($pics as $row) { ?>
 				<div class="col-md-auto">
 					<label style="margin-left: 10px;"><?php echo $row['file']?></label>
@@ -134,7 +199,7 @@ if ($_POST['msg_submit'] === "Post" && $_POST['comment']) {
 									<tr>
 										<script>
 											function myFunction(obj) {
-											obj.outerHTML = '<td colspan="2" align="center"><br><textarea name="comment" id="comment" maxlength="255" placeholder="Add a comment here..."></textarea></td></tr><tr><td colspan="2" align="center"><br><input type="submit" class="btn btn-light" value="Post" name="msg_submit" id="msg_submit" /></td></tr>';
+												obj.outerHTML = '<td colspan="2" align="center"><br><textarea name="comment" id="comment" maxlength="255" placeholder="Add a comment here..."></textarea></td></tr><tr><td colspan="2" align="center"><br><input type="submit" class="btn btn-light" value="Post" name="msg_submit" id="msg_submit" /></td></tr>';
 											}
 										</script>
 										<button class="btn btn-light" onclick="myFunction(this)">Comment</button>
@@ -155,6 +220,11 @@ if ($_POST['msg_submit'] === "Post" && $_POST['comment']) {
 			<?php
 			} ?>
 		</div>
+
+
+		<!-- echo '</div>'; // #gallery end
+
+		echo '<div class="clearfix"></div>'; // The clearfix -->
 
 		<?php require_once 'includes/footer.php';?>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>

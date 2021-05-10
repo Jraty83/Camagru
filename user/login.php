52d093d9dbd6b2f8842b205127acff6488e07fc7
jraty <jraty@student.hive.fi>
2021-05-10 16:34:18 +0300

    navbar collapse (.js) removed

--- user/login.php
+++ user/login.php
@@ -60,6 +60,5 @@ if ($existing_user && $verified == 2) {
 		</div>
 
 		<?php require_once '../includes/footer.php';?>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 	</body>
 </html>
